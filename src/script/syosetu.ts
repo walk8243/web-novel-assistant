7c211aaab139e9c72482f733c0280e8d27eda25b
walk8243 <walk8243@gmail.com>
2025-03-30 20:09:43 +0900

    ページ送り機能を簡単に作成

--- src/script/syosetu.ts
+++ src/script/syosetu.ts
@@ -1,6 +1,31 @@
-document.getElementsByTagName('body')[0].style.backgroundColor = '#f00000';
-
 chrome.runtime.onMessage.addListener((message: unknown, _sender, sendResponse) => {
-	console.log('Received message:', message);
+	console.debug('Received message:', message);
+
+	if (typeof message !== 'object' || message === null || 'command' in message === false) {
+		console.log('Invalid message format');
+		sendResponse({ error: 'Invalid message format', completed: false });
+		return;
+	}
+
+	if (message.command === 'next-page') {
+		const nextPageButton = document.querySelector('.c-pager .c-pager__item--next');
+		if (!nextPageButton || nextPageButton instanceof HTMLAnchorElement === false) {
+			console.log('Next page button not found');
+			sendResponse({ error: 'Next page button not found', completed: false });
+			return;
+		}
+
+		nextPageButton.click();
+	} else if (message.command === 'prev-page') {
+		const previousPageButton = document.querySelector('.c-pager .c-pager__item--before');
+		if (!previousPageButton || previousPageButton instanceof HTMLAnchorElement === false) {
+			console.log('Previous page button not found');
+			sendResponse({ error: 'Previous page button not found', completed: false });
+			return;
+		}
+
+		previousPageButton.click();
+	}
+
 	sendResponse({ site: 'syosetu', completed: true });
 });
